14c5b03738973320f8843af9ff74ccbe8e1113d9
Alexander Panteleev <746590@mail.ru>
2017-04-23 12:37:26 +0300

    add link to find_athletes

--- athletes2008.php
+++ athletes2008.php
@@ -9,6 +9,7 @@
 <center style="line-height:89px;">  <a style="float:none;"class="button1" href="show_athletes2008.php">Показать список участников Олимпиады 2008</a><br>
   <a style="float:none;"  class="button1" href="add_athletes2008.php">Добавить участника</a><br>
   <a style="float:none;" class="button1" href="remove_athletes2008.php">Удалить участника</a><br>
+      <a style="float:none;"  class="button1" href="find_athletes2008.php">Поиск спортсмена</a><br>
 <a href="menu2008.php" class="button"><span>←</span>Назад</a>
 <a  href="index.php" class="button4"><span>⌂</span>В главное меню</a>
   </center>
